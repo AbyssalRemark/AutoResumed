83f606ae0b537dff38c6c99c8cdef596664c66fb
Pope_Rigby <poperigby@mailbox.org>
2024-02-21 13:47:13 -0800

    Endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def main() -> str:
-    return "<p>Hello, world<p>"
+# Bearer token is sent with every request, and check against the DB
+@app.route("/login", methods=["POST", "GET"])
+def login() -> str:
+    print(request)
+    return "Login"
+
+@app.route("/logout")
+def logout() -> str:
+    return "Logout"
+
+@app.route("/register")
+def register() -> str:
+    return "Register"
+
+@app.route("/resume", methods=["GET", "POST", "DELETE"])
+def resume() -> str:
+    # Get JSON of resume
+    if request.method == "GET":
+        return "Resume"
+    # Update the resume in DB
+    elif request.method == "POST":
+        return "Update the resume"
+    # Remove the resume
+    elif request.method == "DELETE":
+        return "Delete the resume"
+    else:
+        return "Else"
