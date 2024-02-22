de063408d61adc489d7ae7e389abd6b2600dd6f5
Pope_Rigby <poperigby@mailbox.org>
2024-02-22 15:33:48 -0800

    Split auth into separate module

--- backend/app.py
+++ backend/app.py
@@ -1,43 +1,12 @@
-from flask import Flask, Response, request
-from flask_json import FlaskJSON, json_response
+from flask import Flask, request
+from flask_json import FlaskJSON
+
+from auth import auth
 
 app = Flask(__name__)
 json = FlaskJSON(app)
 
-# User ID and bearer token are sent with every request, and checked against the DB
-
-# Takes username and password
-# Returns user ID and bearer token
-@app.route("/login", methods=["POST"])
-def login() -> Response:
-    if request.method == "POST":
-        data = request.get_json()
-        print(data)
-
-    # Check database
-
-    # Return user ID and bearer token
-    return json_response(uuid=1234, token=1234)
-
-# Takes user ID and bearer token
-@app.route("/logout", methods=["POST"])
-def logout() -> str:
-    return "Sucessfully logged out"
-
-# Takes username and password
-# Returns user ID and bearer token
-@app.route("/register", methods=["POST"])
-def register() -> Response:
-    if request.method == "POST":
-        data = request.get_json()
-        print(data)
-
-    # Create user entry in database
-
-    # Return user ID and bearer token
-    return json_response(uuid=1234, token=1234)
-
-
+app.register_blueprint(auth, url_prefix="/auth")
 
 @app.route("/resume", methods=["GET", "POST", "DELETE"])
 def resume() -> str:
---
+++ backend/auth.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, Response, request
+from flask_json import json_response
+
+auth = Blueprint("auth", __name__)
+
+# Takes username and password
+# Returns user ID and bearer token
+@auth.route("/login", methods=["POST"])
+def login() -> Response:
+    if request.method == "POST":
+        data = request.get_json()
+        print(data)
+
+    # Check database
+
+    # Return user ID and bearer token
+    return json_response(uuid=1234, token=1234)
+
+# Takes user ID and bearer token
+@auth.route("/logout", methods=["POST"])
+def logout() -> str:
+    return "Sucessfully logged out"
+
+# Takes username and password
+# Returns user ID and bearer token
+@auth.route("/register", methods=["POST"])
+def register() -> Response:
+    if request.method == "POST":
+        data = request.get_json()
+        print(data)
+
+    # Create user entry in database
+
+    # Return user ID and bearer token
+    return json_response(uuid=1234, token=1234)
