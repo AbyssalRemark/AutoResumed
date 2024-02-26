ca0d42af7751cd9c395187510fc2ba1502f68574
Pope_Rigby <poperigby@mailbox.org>
2024-02-26 10:48:44 -0800

    Use dbtool

--- backend/auth.py
+++ backend/auth.py
@@ -1,3 +1,6 @@
+import dbtool
+import app
+
 from flask import Blueprint, Response, request
 from flask_json import json_response
 
@@ -7,7 +10,7 @@ auth = Blueprint("auth", __name__)
 # Takes username and password
 # Returns user ID and bearer token
 @auth.route("/login", methods=["POST"])
-def login() -> Response:
+async def login() -> Response:
     if request.method == "POST":
         data = request.get_json()
         print(data)
@@ -20,19 +23,22 @@ def login() -> Response:
 
 # Takes user ID and bearer token
 @auth.route("/logout", methods=["POST"])
-def logout() -> str:
+async def logout() -> str:
     return "Sucessfully logged out"
 
 
 # Takes username and password
 # Returns user ID and bearer token
 @auth.route("/register", methods=["POST"])
-def register() -> Response:
+async def register() -> Response:
     if request.method == "POST":
         data = request.get_json()
-        print(data)
 
-    # Create user entry in database
+        # Create user entry in database
+        await dbtool.create_user(app.db, data)
 
-    # Return user ID and bearer token
-    return json_response(uuid=1234, token=1234)
+        # Return user ID and bearer token
+        return json_response(uuid=1234, token=1234)
+    else:
+        return json_response(status="401", msg="Bad JSON >:(")
+
