49544896b6d817e3427c001e51a5349e2b520137
Emma O'Leary <oleemm31@evergreen.edu>
2024-03-01 13:05:11 -0800

    Changes made to Login

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -3,39 +3,67 @@ import { redirect, useNavigate } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import { useState } from 'react';
 import PreviousButton from './PreviousButton.jsx';
+//import axios from 'axios';
 
 const LoginPage = () => {
-    const [passwordVisibility, setPasswordVisibility] = useState("password")
-    const [password, setPassword] = useState("")
-    const [email, setEmail] = useState("")
+  const [passwordVisibility, setPasswordVisibility] = useState("password")
+  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("")
 
+  // abc@123.com
+  // password
 
-    const handleClick = () => {
-        useNavigate("/");
+  async function sendLoginRequest(email_input, password_input) {
+
+    //const url = "https://autoresumed.com/auth/login";
+    const url = " http://127.0.0.1:5000/auth/login"
+    const response = await fetch(url, {
+      method: "POST",
+      mode: "cors",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json;",
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify({
+        "email": "abc@123.com",
+        "password": "password"
+      }),
+    });
+    return response.json();
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (email != "" && password != "") {
+      sendLoginRequest(email, password).then((data) => {
+        console.log(data);
+      });
     }
+  };
 
+  return (
 
-    return (
+    <div className="info">
 
-        <div className="info">
+      <PreviousButton></PreviousButton>
+      <Form>
+        <Form.Group className="mb-3" controlId="loginForm.email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </Form.Group>
 
-            <PreviousButton></PreviousButton>
-            <Form>
-                <Form.Group className="mb-3" controlId="loginForm.email">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" value={email} />
-                </Form.Group>
+        <Form.Group className="mb-3" controlId="loginForm.password">
+          <Form.Label>Example textarea</Form.Label>
+          <Form.Control type={passwordVisibility} value={password} onChange={(e) => setPassword(e.target.value)} />
+          <Button variant="danger" type="button" onClick={() => setPasswordVisibility(passwordVisibility == "password" ? "textarea" : "password")}>{passwordVisibility == "password" ? "show" : "hide"}</Button>
+          <Button variant="success" type="submit" onClick={handleSubmit}>Submit</Button>
+        </Form.Group>
 
-                <Form.Group className="mb-3" controlId="loginForm.password">
-                    <Form.Label>Example textarea</Form.Label>
-                    <Form.Control type={passwordVisibility} />
-                    <Button variant="danger" type="button" onClick={() => setPasswordVisibility(passwordVisibility == "password" ? "textarea" : "password")}>{passwordVisibility == "password" ? "show" : "hide"}</Button>
-                    <Button variant="success" type="submit" onClick={handleClick}>Submit</Button>
-                </Form.Group>
-
-            </Form>
-        </div>
-    );
+      </Form>
+    </div>
+  );
 };
 
 export default LoginPage;
