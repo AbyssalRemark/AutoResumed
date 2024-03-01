8a2489a1c94263fafc96713ee39bfb872c1344d1
Pope_Rigby <poperigby@mailbox.org>
2024-03-01 11:47:17 -0800

    Fix shit

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -5,39 +5,37 @@ import { useState } from 'react';
 import PreviousButton from './PreviousButton.jsx';
 
 const LoginPage = () => {
-  const [passwordVisibility, setPasswordVisibility] = useState("password")
-  const [password, setPassword] = useState("")
-  const [email, setEmail] = useState("")
+    const [passwordVisibility, setPasswordVisibility] = useState("password")
+    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
 
 
-  const handleClick = () => {
-    useNavigate("/");
-
-  const 
-  }
+    const handleClick = () => {
+        useNavigate("/");
+    }
 
 
-  return (
+    return (
 
-    <div className="info">
+        <div className="info">
 
-      <PreviousButton></PreviousButton>
-      <Form>
-        <Form.Group className="mb-3" controlId="loginForm.email">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" value={email}/>
-        </Form.Group>
+            <PreviousButton></PreviousButton>
+            <Form>
+                <Form.Group className="mb-3" controlId="loginForm.email">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="name@example.com" value={email} />
+                </Form.Group>
 
-        <Form.Group className="mb-3" controlId="loginForm.password">
-          <Form.Label>Example textarea</Form.Label>
-          <Form.Control type={passwordVisibility} />
-          <Button variant="danger" type="button" onClick={() => setPasswordVisibility(passwordVisibility == "password" ? "textarea" : "password")}>{passwordVisibility == "password" ? "show" : "hide"}</Button>
-          <Button variant="success" type="submit" onClick={handleClick}>Submit</Button>
-        </Form.Group>
+                <Form.Group className="mb-3" controlId="loginForm.password">
+                    <Form.Label>Example textarea</Form.Label>
+                    <Form.Control type={passwordVisibility} />
+                    <Button variant="danger" type="button" onClick={() => setPasswordVisibility(passwordVisibility == "password" ? "textarea" : "password")}>{passwordVisibility == "password" ? "show" : "hide"}</Button>
+                    <Button variant="success" type="submit" onClick={handleClick}>Submit</Button>
+                </Form.Group>
 
-      </Form>
-    </div>
-  );
+            </Form>
+        </div>
+    );
 };
 
 export default LoginPage;
