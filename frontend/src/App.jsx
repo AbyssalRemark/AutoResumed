c3304e64cc589cc74080f713f0fb7f55469fc4a1
Emma O'Leary <oleemm31@evergreen.edu>
2024-03-01 10:28:32 -0800

    Committing changes to prepare for team meeting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,16 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Welcome from "./Welcome.jsx"
 import LoginPage from "./LoginPage.jsx"
 import RegistrationPage  from "./RegistrationPage.jsx"
+import Layout from "./Layout.jsx"
 import 'bootstrap/dist/css/bootstrap.css';
+
 const App = () => {
   return (
 <BrowserRouter>
-  <h1>AutoResumed</h1>
-  <Link to="/login">login</Link>
-  {` `}
-  <Link to="/register">register</Link>
+<Layout></Layout>
   <Routes>
-    <Route path="/welcome" element={<Welcome />} />
+  <Route path="/" element={<Welcome />} />
+    <Route path="/welcome"  element={<Welcome />} />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/register" element={<RegistrationPage />} />
   </Routes>
