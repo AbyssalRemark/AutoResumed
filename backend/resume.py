46443d4d00ac6cb0533ff4f1c42fbb30d3305053
Pope_Rigby <poperigby@mailbox.org>
2024-03-06 14:21:37 -0800

    Add generate endpoint

--- backend/resume.py
+++ backend/resume.py
@@ -35,3 +35,31 @@ async def update():
         )
 
     return "Fine"
+
+@resume.route("/generate", methods=["POST"])
+async def generate():
+    """
+    Generate an HTML resume, given a token, a set of tags, and a resume template.
+    """
+
+    data = request.get_json()
+
+    try:
+       token = str(data["token"])
+       tags = list(data["tags"])
+       template = str(data["template"])
+    except (KeyError, TypeError, ValueError):
+        raise JsonError(
+            400,
+            error="invalid-json",
+            message="Invalid JSON data.",
+            detail="We expect { 'token': '<bearer-token>', 'tags': [ '<tag>', '<tag>', ... ], 'template': '<resume-template>' }.",
+        )
+
+    # tagged_resume = dbtool.get_resume(token)
+    # flattened_resume = flattener.parse(tagged_resume, tags)
+    # html_resume = resumed.to_html(flattened_resume, template)
+
+    # return html_resume
+
+    return "HTML resume"
