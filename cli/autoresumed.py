00891f80782135254a427bc31102630d69d41bfc
AbyssalRemark <dominicsevere@gmail.com>
2024-03-06 13:47:20 -0800

    genResume implimented, needs testing, should be useable

--- cli/autoresumed.py
+++ cli/autoresumed.py
@@ -19,7 +19,35 @@ resumes off of taged entries for faster resume making.
 import os
 import json
 from pprint import pprint as pprint
+from typing import Final
+from copy import deepcopy #python shallow copies by default. 
 
+#const fields for talking about whats in the resume
+CONST_FIELDS: Final = ["basics", "work", "volunteer", "education", "awards", "certificates", 
+                       "publications", "skills", "languages", "interests", "references", "projects"]
+
+#==============================================================================#
+#
+#   Resume translating functions
+#
+#==============================================================================#
+
+# creates a Resume 
+def genResume(js, tags):
+    res = parseResume(js, tags)
+    res = stripTags(js)
+    return res
+
+#==============================================================================#
+#
+#   Resume editing and creation
+#
+#==============================================================================#
+
+#not currently my job but will be later..
+
+
+#some notes for myself
 #not responsible for createion...?
 #add
 #update
@@ -34,38 +62,47 @@ from pprint import pprint as pprint
 
 #insertion into work history needs to be chronological I think but idk.
 
+#==============================================================================#
+#
+#   Private things you can use but probably shouldn't
+#
+#==============================================================================#
 #for lists
 def parseAll(list, tags): #knowing python this can probasbly be one line but idk
     parsed = [] 
     for i in list:
         for j in tags:
             if j in i["tags"]: #any tag found
-                parsed.append(i)
+                parsed.append(deepcopy(i))
     return parsed
 
 def parseFirst(list, tags):
     parsed = [] 
+    pprint(list)
+    print(type(list))
     for i in list:
         for j in tags:
             if j in i["tags"]: #any tag found
-                parsed.append(i)
+                parsed.append(deepcopy(i))
                 break #same logic as above but returns the first one
     return parsed
 
-#assumes its been loaded correctly. (this can be changed)
+#assumes its been loaded correctly.
+#   Does not remove tags. 
+#   Cassody you probably want genResume not this.
+#   *Should* not modify js in any way
 def parseResume(js, tags):
+    #where the new resume goes
     res = {}
     # "education", is not tagged and needs to be pulled seperately. 
-    fields = ["basics", "work", "volunteer", "education", "awards", "certificates", 
-              "publications", "skills", "languages", "interests", "references", "projects"]
-
-    for field in (fields):
+    for field in (CONST_FIELDS):
+        # "education", is not tagged and needs to be pulled seperately. 
         match(field):
             case "basics":
                 #grabs tagged single fields because there special
 
-                #copys all entry to avoid indevidual copying
-                basics = js["basics"] #for easier reading further down
+                #copys all entry to avoid indevidual copying, deep copy
+                basics = deepcopy(js["basics"]) #for easier reading further down
                 res["basics"] = basics
 
                 #grabs correct tag (hence why we may need default to be special)
@@ -78,15 +115,33 @@ def parseResume(js, tags):
 
                 #profiles are not a grab one its a grab all that apply
                 res["basics"]["profiles"] = parseAll(basics["profiles"], tags)
-
             case "work" | "volunteer" | "awards" | "certificates" | "publications" | "skills" | "languages" | "interests" | "references" | "projects":
                 #grabs all tags that apply. 
+                #not a fan of all those | but it is correct.
                 print("parse all for " + field)
                 res[field] =  parseAll(js[field], tags)
             case _:
-                #default behavior, copy all fields, these are not tagged.
+                #default behavior, copy all fields, these are not tagged. like education
                 print("default for " + field)
                 res[field] = js[field]
+    return res
+
+def stripTags(js):
+    #sense python flip flops on weather things are passed by value or refference 
+    #were not taking any chances with how this is used so we copy it manually
+    #just in case to prevent the original from being stripped. 
+    res = deepcopy(js)
+    for field in CONST_FIELDS:
+        match(field):
+            case "basics":
+                #basics is special but only a subfield profiles will still have tags
+                for profile in res["basics"]["profiles"]:
+                    del profile["tags"]
+            case "work" | "volunteer" | "awards" | "certificates" | "publications" | "skills" | "languages" | "interests" | "references" | "projects":
+                for entry in res[field]:
+                    del entry["tags"]
+            #case _:
+                #default behavior does nothing. as these are all untagged fields
     return res
 
 if __name__ == "__main__":
@@ -94,8 +149,13 @@ if __name__ == "__main__":
         js = json.load(file)
     #pprint(js, sort_dicts=False)
     #print(type(js))
-
-    work = js["work"]
     #pprint(parseAll(work, ["craft"]))
     #pprint(parseFirst(js["basics"]["label"],["tech"]))
-    pprint(parseResume(js,["tech"]), sort_dicts=False)
+    #parsed = parseResume(js,["tech"])
+    #pprint(parsed, sort_dicts=False)
+    #pprint(stripTags(parsed), sort_dicts=False)
+    pprint(genResume(js, ["tech"]), sort_dicts=False)
+    #print("")
+    #print("")
+    #print("")
+    pprint(genResume(js, ["craft"]), sort_dicts=False)
