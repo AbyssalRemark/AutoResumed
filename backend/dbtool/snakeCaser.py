192422f6f6b15dd973b6f8de8762198b3e77025f
Quinn Allen <aquinnallen@github.com>
2024-03-08 21:13:43 -0800

    resume now comes in nice clean camelCase keyed json, handling snakes in the background..get_resume is final done, sha256 change also implimented

--- backend/dbtool/snakeCaser.py
+++ backend/dbtool/snakeCaser.py
@@ -1,18 +1,19 @@
 import re
 
 def convert_to_snake(data):
-    required_dict = {}
+    snake_dict = {}
     for key, value in data.items():
-        if isinstance(value, str):
-            new_key = re.sub("([a-z0-9])([A-Z])", r"\1_\2", key).lower()
-            required_dict[new_key] = value
-        elif isinstance(value, list) and all(isinstance(item, str) for item in value):
-            new_key = re.sub("([a-z0-9])([A-Z])", r"\1_\2", key).lower()
-            required_dict[new_key] = value
-        elif isinstance(value, list) and all(isinstance(item, dict) for item in value):
-            new_key = re.sub("([a-z0-9])([A-Z])", r"\1_\2", key).lower()
-            required_dict[new_key] = [convert(item) for item in value]
-    return required_dict
+        new_key = re.sub("([a-z0-9])([A-Z])", r"\1_\2", key).lower()
+        if isinstance(value, dict):
+            snake_dict[new_key] = convert_to_snake(value)
+        elif isinstance(value, list):
+            if all(isinstance(item, dict) for item in value):
+                snake_dict[new_key] = [convert_to_snake(item) for item in value]
+            elif all(isinstance(item, str) for item in value):
+                snake_dict[new_key] = value
+        else:
+            snake_dict[new_key] = value
+    return snake_dict
 
 if __name__ == "__main__":
     # Example usage:
@@ -25,6 +26,8 @@ if __name__ == "__main__":
         },
         'phoneNumbers': ['123-456-7890', '987-654-3210']
     }
+    _local_dict = {"name": "John Doe","label": [{"tags":["tag"],"label":"Programmer"}],"image": "https://somesite.tld/img.png","email": "john@gmail.com","phone": "(912) 555-4321","url": "https://johndoe.com","summary": [{"tags":["tag"],"summary":"A summary of John Doe…"}],"location": {"address": "2712 Broadway St","postal_code": "CA 94115","city": "San Francisco","countryCode": "US","region": "California"},"profiles": [{"tags": ["tag"],"network": "Twitter","username": "john","url": "https://twitter.com/john"}]}
 
-    snake_case_dict = convert(camel_case_dict)
+    snake_case_dict = convert_to_snake(camel_case_dict)
     print(snake_case_dict)
+    print(convert_to_snake(_local_dict))
