5d73adfd9853e7cfdaf1b882e59c5b5c97a97d48
Pope_Rigby <poperigby@mailbox.org>
2024-03-11 16:15:35 -0700

    I made it pretty

--- frontend/src/Utils/Layout.jsx
+++ frontend/src/Utils/Layout.jsx
@@ -4,23 +4,23 @@ import { Nav, Navbar, NavDropdown, Container, Button } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.css';
 
 const Layout = () => {
-  /*return (
-<BrowserRouter>
-  <h1>AutoResumed</h1>
-  <Link to="/login">login</Link>
-  {` `}
-  <Link to="/register">register</Link>
-  <Routes>
-    <Route path="/" element={<Layout />} />
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/register" element={<RegistrationPage />} />
-  </Routes>
-</BrowserRouter>
-  );*/
-  return (
-    <div>
-      <style type="text/css">
-        {`
+    /*return (
+  <BrowserRouter>
+    <h1>AutoResumed</h1>
+    <Link to="/login">login</Link>
+    {` `}
+    <Link to="/register">register</Link>
+    <Routes>
+      <Route path="/" element={<Layout />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegistrationPage />} />
+    </Routes>
+  </BrowserRouter>
+    );*/
+    return (
+        <div>
+            <style type="text/css">
+                {`
     body {
       background-color: rgb(255, 241, 143);
     }
@@ -31,28 +31,28 @@ const Layout = () => {
     }
     `}</style>
 
-      <Navbar expand="lg" className="bg-body-tertiary" >
-        <Container>
-          <Nav fill variant="tabs" defaultActiveKey={"/"}>
-            <h1>Autoresumed</h1>
-            <Nav.Item>
-              <Link to="/login"><Button variant="outline-danger">login</Button></Link>
-            </Nav.Item>
-            {` `}
-            <Nav.Item>
-              <Link to="/register"><Button variant="outline-danger">register</Button></Link>
-            </Nav.Item>
+            <Navbar expand="lg" className="bg-body-tertiary" >
+                <Container>
+                    <Nav fill variant="tabs" defaultActiveKey={"/"}>
+                        <h1>AutoResumed</h1>
+                        <Nav.Item>
+                            <Link to="/login"><Button variant="outline-danger">login</Button></Link>
+                        </Nav.Item>
+                        {` `}
+                        <Nav.Item>
+                            <Link to="/register"><Button variant="outline-danger">register</Button></Link>
+                        </Nav.Item>
 
 
-            <Nav.Item>
-              <Link to="/create"><Button variant="outline-danger">create</Button></Link>
-            </Nav.Item>
-          </Nav>
-        </Container>
+                        <Nav.Item>
+                            <Link to="/create"><Button variant="outline-danger">create</Button></Link>
+                        </Nav.Item>
+                    </Nav>
+                </Container>
 
-      </Navbar>
-    </div>
-  );
+            </Navbar>
+        </div>
+    );
 };
 
 export default Layout
