682085f75f54c6b1d518dac1a445ae138cdeefa6
Pope_Rigby <poperigby@mailbox.org>
2024-03-11 16:03:40 -0700

    Fixtures!

--- backend/route_test.py
+++ backend/route_test.py
@@ -1,3 +1,4 @@
+from flask import Flask
 import pytest
 from app import create_app
 
@@ -5,3 +6,11 @@ from app import create_app
 def app_dict():
     app = create_app()
     app.config.update({"TESTING": True})
+
+@pytest.fixture()
+def client(app: Flask):
+    return app.test_client()
+
+@pytest.fixture()
+def runner(app: Flask):
+    return app.test_cli_runner()
