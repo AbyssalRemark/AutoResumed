d5886ee9126e647626bfe6d9bb1922db80b1a81e
Pope_Rigby <poperigby@mailbox.org>
2024-03-11 16:24:31 -0700

    Stuff

--- backend/route_test.py
+++ backend/route_test.py
@@ -4,7 +4,7 @@ import pytest
 from app import create_app
 
 @pytest.fixture(autouse=True, scope="session")
-def app_dict():
+def app():
     app = create_app()
     app.config.update({"TESTING": True})
 
