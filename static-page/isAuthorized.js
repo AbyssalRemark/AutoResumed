9c22075603941de791af4db1bed2f2a02f9578e9
Quinn Allen <aquinnallen@github.com>
2024-03-12 15:36:41 -0700

    isAuthorized.js

---
+++ static-page/isAuthorized.js
@@ -0,0 +1,14 @@
+export async function isAuthorized(){
+    let token = {'token':localStorage.getItem('token')};
+    let isAuthorized = await fetch("http://0.0.0.0/auth/is_authorized",{
+        method:"POST",
+        body: JSON.stringify(token)
+    });
+    if (isAuthorized.ok){
+        let ret = (await isAuthorized.json())["authorized"];
+        return ret
+    }
+    else{
+        return False;
+    }
+}
