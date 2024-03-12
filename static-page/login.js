7f71eec1e09e5e4a2588631eca94bc01411a5d80
Pope_Rigby <poperigby@mailbox.org>
2024-03-12 15:23:57 -0700

    Hmm

--- static-page/login.js
+++ static-page/login.js
@@ -25,21 +25,6 @@ async function login(email, password) {
     }
 }
 
-// async function logout(token) {
-//     await fetch(
-//         "http://127.0.0.1:5000/auth/logout",
-//         {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify({ "token": token })
-//         }
-//     );
-//
-//     localStorage.removeItem("token");
-// }
-//
 // async function _delete(token) {
 //     await fetch(
 //         "http://127.0.0.1:5000/auth/delete",
