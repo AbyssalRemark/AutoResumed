cc4a32e626e1656eeb85e4fb4adb48339cf9e2f2
Pope_Rigby <poperigby@mailbox.org>
2024-03-12 14:42:46 -0700

    Things

--- static-page/login.js
+++ static-page/login.js
@@ -7,19 +7,6 @@ document.getElementById("loginForm").addEventListener("submit", async function(e
     await login(email, password);
 });
 
-// async function register(email, password) {
-//     await fetch(
-//         "http://127.0.0.1:5000/auth/register",
-//         {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify({ "email": email, "password": password })
-//         }
-//     );
-// }
-
 async function login(email, password) {
     const response = await fetch(
         "http://127.0.0.1:5000/auth/login",
@@ -32,7 +19,7 @@ async function login(email, password) {
         }
     );
 
-    const token = response.json()["token"];
+    const token = await response.json()["token"];
     localStorage.setItem("token", token);
 }
 
