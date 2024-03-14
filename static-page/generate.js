03ce9a62fab7da81c4f9260f626c2409fb6fd6c8
Pope_Rigby <poperigby@mailbox.org>
2024-03-13 21:56:35 -0700

    It works for some reason

--- static-page/generate.js
+++ static-page/generate.js
@@ -3,10 +3,16 @@ document.getElementById("generate-button").addEventListener("click", async funct
 });
 
 document.getElementById("download-html-button").addEventListener("click", async function() {
-    // document.getElementById("resume").srcdoc = await generate();
+    html = await generate("html");
+    downloadHtml("resume.html", html);
 });
 
-async function generate() {
+document.getElementById("download-pdf-button").addEventListener("click", async function() {
+    pdf = await generate("pdf");
+    downloadPdf("resume.pdf", pdf);
+});
+
+async function generate(type) {
     const token = localStorage.getItem("token");
     const tags = ["tech"]; // TODO: Unhardcode
     const template = document.getElementById("template-selector").value;
@@ -22,13 +28,45 @@ async function generate() {
                 "token": token,
                 "tags": tags,
                 "template": template,
-                "type": "html",
+                "type": type,
             })
         }
     );
 
-    if (response.ok) {
+    if (response.ok && type == "html") {
         return (await response.json())["resume"];
+    } else if (response.ok && type == "pdf") {
+        return await response.blob()
     }
 
+}
+
+function downloadHtml(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+function downloadPdf(filename, pdfData) {
+    var blob = new Blob([pdfData], { type: 'application/pdf' });
+    var url = URL.createObjectURL(blob);
+
+    var element = document.createElement('a');
+    element.setAttribute('href', url);
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+    URL.revokeObjectURL(url); // Clean up the URL object to free resources
 }
