8a17ac7c461a366ab45b713698575c19f3cbca79
Pope_Rigby <poperigby@mailbox.org>
2024-03-14 15:17:14 -0700

    Flask debug

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,8 @@
-import waitress
+# import waitress
 from app import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    waitress.serve(app, host="0.0.0.0", port=42069)
+    # waitress.serve(app, host="0.0.0.0", port=42069)
+
+    app.run(debug=True, port=42069)
